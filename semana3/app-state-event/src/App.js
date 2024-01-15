b0c2e3995230ebd46acdd639881e9f0dfd6d00dd
HAjiMe-0 <140862936+HAjiMe-0@users.noreply.github.com>
2024-01-15 15:53:50 +0000

    gg

--- semana3/app-state-event/src/App.js
+++ semana3/app-state-event/src/App.js
@@ -5,24 +5,33 @@ import { useState } from 'react';
 
 function App() {
   const [nroClicks, setNumClicks] = useState(0);
-  const click = () => {
-    setNumClicks(nroClicks + 1);
-    console.log('click')
+  const clickmenos3 = () => {
+    setNumClicks(nroClicks - 3);
   }
+  const clickmas3 = () => {
+    setNumClicks(nroClicks + 3);
+  }
+  const clickmenos1 = () => {
+    setNumClicks(nroClicks - 1);
+  }
+  const clickmas1 = () => {
+    setNumClicks(nroClicks + 1);
+  }
+  
   const reiniciar = () => {
     setNumClicks(0);
-    console.log('r')
   }
   return (
     <div className="App">
       <div className="contenedor-principal">
         <Contador nroClicks={nroClicks} />
         <div className='contendedor-botones'>
-          <Boton texto="Click" esBotonClick={true}
-            funcionClick={click} />
-          <Boton texto="Reiniciar" esBotonClick={false}
-            funcionClick={reiniciar} />
+          <Boton texto="-3" esBotonClick={true} funcionClick={clickmenos3} />
+          <Boton texto="+3" esBotonClick={true} funcionClick={clickmas3} />
+          <Boton texto="-1" esBotonClick={true}funcionClick={clickmenos1} />
+          <Boton texto="+1" esBotonClick={true}funcionClick={clickmas1} />
         </div>
+        <Boton texto="Reiniciar" esBotonClick={false}funcionClick={reiniciar} />
       </div>
     </div>
   );
