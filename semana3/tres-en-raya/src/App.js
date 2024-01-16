f7136e245fd1808e280999147df1e9679e65655b
HAjiMe-0 <140862936+HAjiMe-0@users.noreply.github.com>
2024-01-16 14:56:14 +0000

    gg

---
+++ semana3/tres-en-raya/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+//import Tablero from './components/Tablero.js';
+import TableroB from './components/TableroB';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <TableroB/>
+    </div>
+  );
+}
+
+export default App;
